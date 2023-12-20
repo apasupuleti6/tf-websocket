f375d038539ce0f7e6d5b6f278178585c27d6163
Aswini Pasupuleti <aswinipasupuleti2@gmail.com>
2023-12-20 15:38:53 -0600

    updating the index function to connect to dynamodb

--- index.mjs
+++ index.mjs
@@ -1,9 +1,24 @@
-export const handler = async (event) => {
-    // TODO implement
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+export const handler = async (event, context) => {
+  console.log("The event is: "+ JSON.stringify(event));
+  const command = new PutCommand({
+    TableName: "websocket-connection",
+    Item: {
+      clientId:" testClientInsert",
+      connectionId: event['requestContext']['connectionId']
+    },
+  });
+
+  const response = await docClient.send(command);
+  console.log("came out of successful creation of table insert");
+  console.log(response);
+  const return_response = {
+    statusCode : 200
   };
-  
+  return JSON.stringify(return_response);
+};
