d10994e0ab539ae6e0247b727961f2e3233fbe57
Aswini Pasupuleti <aswinipasupuleti2@gmail.com>
2023-12-22 11:36:33 -0600

    $disconnect function for websocket connection

---
+++ disconnect.mjs
@@ -0,0 +1,19 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, DeleteCommand } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+export const handler = async () => {
+  const command = new DeleteCommand({
+    TableName: "websocket-connection",
+    Key: {
+      clientId: "testClientInsertola",
+    },
+  });
+
+  const response = await docClient.send(command);
+  console.log(response);
+  return response;
+};
+
