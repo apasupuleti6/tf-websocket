cf6eb3be44c80d520eb41026fd35bfbe0d0d9ace
Aswini Pasupuleti <aswinipasupuleti2@gmail.com>
2024-01-03 18:28:38 -0600

    renaming integrations

--- websocket-aipgateway.tf
+++ websocket-aipgateway.tf
@@ -33,20 +33,21 @@ resource "aws_apigatewayv2_stage" "dev" {
 
 
 
-resource "aws_apigatewayv2_route" "apigateway-route" {
+resource "aws_apigatewayv2_route" "apigateway_route_default" {
   api_id    = aws_apigatewayv2_api.websocket-gw.id
   route_key = "$default"
-  target    = "integrations/${aws_apigatewayv2_integration.gw_route_c.id}"
+  target    = "integrations/${aws_apigatewayv2_integration.connect_lamda.id}"
 }
+
 
 /* route and integration for $connect */
 resource "aws_apigatewayv2_route" "apigateway-route-c" {
   api_id    = aws_apigatewayv2_api.websocket-gw.id
   route_key = "$connect"
-  target    = "integrations/${aws_apigatewayv2_integration.gw_route_c.id}"
+  target    = "integrations/${aws_apigatewayv2_integration.connect_lamda.id}"
 }
 
-resource "aws_apigatewayv2_integration" "gw_route_c" {
+resource "aws_apigatewayv2_integration" "connect_lamda" {
   api_id           = aws_apigatewayv2_api.websocket-gw.id
   integration_uri  = aws_lambda_function.websocket_lambda_connect.invoke_arn
   integration_type = "AWS_PROXY"
@@ -56,10 +57,10 @@ resource "aws_apigatewayv2_integration" "gw_route_c" {
 resource "aws_apigatewayv2_route" "apigateway-route-d" {
   api_id    = aws_apigatewayv2_api.websocket-gw.id
   route_key = "$disconnect"
-  target    = "integrations/${aws_apigatewayv2_integration.gw_route_d.id}"
+  target    = "integrations/${aws_apigatewayv2_integration.disconnect_lamda.id}"
 }
 
-resource "aws_apigatewayv2_integration" "gw_route_d" {
+resource "aws_apigatewayv2_integration" "disconnect_lamda" {
   api_id           = aws_apigatewayv2_api.websocket-gw.id
   integration_uri  = aws_lambda_function.websocket_lambda_disconnect.invoke_arn
   integration_type = "AWS_PROXY"
